26315220deb025a40265f910e2f4dd8703cba30b
xubor <xubor007@live.de>
2019-10-16 22:57:03 +0200

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+/* Includes */
+#include <stddef.h>
+#include <stdio.h>
+#include "stm32f10x.h"
+
+int main(void)
+{
+
+  while (1)
+  {
+  }
+  return 0;
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/*
+ * Minimal __assert_func used by the assert() macro
+ * */
+extern "C" void __assert_func(const char *file, int line, const char *func, const char *failedexpr)
+{
+  while(1)
+  {}
+}
+
+/*
+ * Minimal __assert() uses __assert__func()
+ * */
+extern "C" void __assert(const char *file, int line, const char *failedexpr)
+{
+   __assert_func (file, line, NULL, failedexpr);
+}
+
